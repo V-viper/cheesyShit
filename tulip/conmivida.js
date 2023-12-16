106dde7e90c7fdfae21ce3eebceb04ea47c73699
Ina-moon <cyryxnyx@gmail.com>
2023-12-16 11:49:17 -0400

    a

--- tulip/conmivida.js
+++ tulip/conmivida.js
@@ -1,34 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
   let randomX, randomY, randomZ;
   let ogBug, container;
-  let cloneCount = 10;
-  
+  let cloneCount = 8;
+
   // Update Firefly position
   function randomPos(element) {
     randomX = Math.floor(Math.random() * window.innerWidth);
-    randomY = Math.floor(Math.random() * (window.innerHeight - 300));
+    randomY = Math.floor(Math.random() * window.innerHeight);
     randomZ = Math.floor(Math.random() * 700);
-
+    
     element.style.transform = `translate3d(${randomX}px, ${randomY}px, ${randomZ}px)`;
   }
-  
+
+  function bugAnimation(element){
+    element.animate([
+      { opacity: 1 },
+      { opacity: 0.8},
+      { opacity: 0.5 },
+      { opacity: 0 },
+      { opacity: 0.5 },
+      { opacity: 0.8},
+    ], {
+      duration: 8000,
+      iterations: Infinity
+    }
+    );
+  }
   // Create new Fireflies, apply initial positioning, change position in intervals
   function createAndMoveDiv() {
     ogBug = document.querySelector('#bug');
     container = document.body;
-    
+
     let cloneBug = ogBug.cloneNode(true);
     container.appendChild(cloneBug);
 
     // Set initial position
     randomPos(cloneBug);
-    
+
     setInterval(function () {
       randomPos(cloneBug);
+      bugAnimation(cloneBug);
 
-    }, Math.random() * 8000); 
+    }, Math.random() * 8000);
+
   }
-
   // Clone the div, apply positioning, and append it to the container
   for (let i = 0; i < cloneCount; i++) {
     createAndMoveDiv();
@@ -36,27 +51,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-function txtPopUp(){  
+function txtPopUp() {
   var txt = document.getElementsByClassName('txt-container');
   var txt2 = document.getElementsByClassName('txt-container2');
-   
-  var op = 0;
 
-  if(txt[0].style.opacity === "0"){
+  if (txt[0].style.opacity === "0") {
     txt[0].style.opacity = '1';
     txt2[0].style.opacity = '1';
 
-  } else{
+  } else {
     txt[0].style.opacity = '0';
     txt2[0].style.opacity = '0';
-  } 
+  }
 
 }
 
-function tooltipHi(){
+function tooltipHi() {
   document.getElementById("tooltip").style.visibility = "visible";
 }
 
-function tooltipBye(){
+function tooltipBye() {
   document.getElementById("tooltip").style.visibility = "hidden";
 }
