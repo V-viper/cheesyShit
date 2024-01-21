4ad71396fde3c3c00a1ecedeb5c7047f27056e39
ina-moon <cyryxnyx@gmail.com>
2024-01-21 00:27:53 -0600

    si

---
+++ loveLetter/script.js
@@ -0,0 +1,19 @@
+function openLetter() {
+    let letter = document.getElementById("letter-open");
+    let envelope = document.getElementById("closedSect");
+
+    let checkDisplay = window.getComputedStyle(letter).display;
+
+    checkDisplay === "none" ? letter.style.display = "block" : letter.style.display = "none";
+    letter.style.display === "block" ? envelope.style.display = "none" : envelope.style.display = "block";
+
+}
+
+function noti() {
+    let notification = document.getElementById("notification");
+
+    notification.style.opacity = 0;
+}
+
+
+  
